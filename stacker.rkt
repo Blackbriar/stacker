0dcbf4a3c66c8b020198f86b31cf03774c6a314f
AgtLucas <lucas@agtlucas.com>
2018-07-02 20:55:16 +0200

    Update stacker

--- stacker.rkt
+++ stacker.rkt
@@ -1,1 +1,10 @@
-#lang br
+#lang br/quicklang
+
+; setup read-syntax function with 2 arguments
+(define (read-syntax path port)
+  (define src-lines (port->lines port))
+  (datum->syntax #f '(module lucy br
+                 42)))
+
+; export read-syntax function
+(provide read-syntax)
