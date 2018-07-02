8ebdb168a8e684a5f3683a9a6e368a4d293ad7dc
AgtLucas <lucas@agtlucas.com>
2018-07-02 21:08:56 +0200

    Update read-syntax function

--- stacker.rkt
+++ stacker.rkt
@@ -2,9 +2,11 @@
 
 ; setup read-syntax function with 2 arguments
 (define (read-syntax path port)
-  (define src-lines (port->lines port))
-  (datum->syntax #f '(module lucy br
-                 42)))
+  (define src-lines (port->lines port)) ; convert port contents to a list of strings
+  (define src-datums (format-datums '(handle ~a) src-lines))
+  (define module-datum `(module stacker-mod "stacker.rkt"
+                          ,@src-datums))
+  (datum->syntax #f module-datum))
 
 ; export read-syntax function
 (provide read-syntax)
